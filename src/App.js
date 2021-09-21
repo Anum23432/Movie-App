054a193e681de89bc1cc6ab225db057e4fa3562d
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-09-21 09:00:07 +0500

    Movie App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SearchMovie from './components/SearchMovie';
+function App() {
+  return (
+    <div className="container">
+    <h1 className='title'>React Movie Search</h1>
+    <SearchMovie/>
+  </div>
+  );
+}
+
+export default App;
